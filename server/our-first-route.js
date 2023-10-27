6674d1f1f409da867b0a0c89510269b774016aba
Thomas Card <tcardlab@gmail.com>
2023-10-27 02:19:43 -0400

    Trial: fastify cli removes some boiler plate tho the --watch feature seems a tad slow

--- server/our-first-route.js
+++ server/our-first-route.js
@@ -1,7 +1,7 @@
 import { db } from "./db/index.js";
 
 async function routes(fastify, options) {
-  fastify.get("/", async (request, reply) => {
+  fastify.get("/db", async (request, reply) => {
     const allUsers = await db.query.applications.findMany();
     console.log("test", allUsers);
     return { hello: "world" };
--- server/server.js
+++ server/server.js
@@ -1,41 +1,23 @@
-import Fastify from "fastify";
+/*
+  npm start -p=5000 -w
+  `fastify help` start for more info
+*/
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import firstRoute from "./our-first-route.js";
 import { db } from "./db/index.js";
 
-const fastify = Fastify({
-  logger: true,
-});
+export default async function (fastify, opts) {
+  console.log('Hit')
 
-// Declare a route
-// fastify.get("/", function (request, reply) {
-//   reply.send({ hello: "world" });
-// });
-
-const main = async () => {
-  // console.log("db", db);
-  // await migrate(db, { migrationsFolder: "./migrations" })
-  //   .then(() => {
-  //     console.log("Migrations complete!");
-  //     process.exit(0);
-  //   })
-  //   .catch((err) => {
-  //     console.error("Migrations failed!", err);
-  //     process.exit(1);
-  //   });
+  /* 
+  // i don't have .env set up
   await migrate(db, {
     migrationsFolder: "./migrations",
-  });
+  }); */
 
   await fastify.register(firstRoute);
 
-  // Run the server!
-  await fastify.listen({ port: 8080 }, function (err, address) {
-    if (err) {
-      fastify.log.error(err);
-      process.exit(1);
-    }
-    // Server is now listening on ${address}
-  });
-};
-main();
+  fastify.get('/', async (request, reply) => {
+    return { hello: 'Hello' }
+  })
+}
