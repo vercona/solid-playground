853d4537286a8777dac7ac11f097ec7a08dd8431
Thomas Card <tcardlab@gmail.com>
2023-11-15 03:29:15 -0500

    Chore: clean

--- client/src/pages/Post.tsx
+++ client/src/pages/Post.tsx
@@ -1,5 +1,5 @@
 // Solid Imports
-import { Show, createResource, ErrorBoundary, For, createEffect, Signal, Accessor } from "solid-js";
+import { Show, createResource, ErrorBoundary, For, createEffect, Signal, Accessor, Setter } from "solid-js";
 import { Navigate, useNavigate, useParams } from "@solidjs/router";
 
 // API Imports
@@ -16,32 +16,26 @@ import { createStore, reconcile, unwrap } from "solid-js/store";
 import type { Store, SetStoreFunction } from "solid-js/store";
   
   
-function toSignal<T>(store:Store<T[]>, setStore: SetStoreFunction<T[]>) {
-  function createDeepSignal<T>(): Signal<T | undefined>;
-  function createDeepSignal<T>(value: T): Signal<T>;
-  function createDeepSignal<T>(v?: T): Signal<T> {
-    return [
-      () => store[0] as Accessor<T>,
+import type { ResourceSource, ResourceFetcher, InitializedResource, Resource } from "solid-js";
+function resourceStore<T, S>(watcher:ResourceSource<S>, fetcher:ResourceFetcher<S, T, unknown>, options={}) {
+  const [store, setStore] = createStore<PostAndComments[]>([]);
+
+  function toSignal<T>(v?: T) { 
+    return ([
+      () => store[0],
       (update: T) => (
         setStore(0, reconcile(typeof update === "function" ? update(unwrap(store[0])) : v))
-      ),
-    ] as Signal<T>;
+      )
+    ] as Signal<T | undefined>)
   }
-
-  return createDeepSignal
-}
-
-import type { ResourceSource, ResourceFetcher, InitializedResource, Resource } from "solid-js";
-function resourceStore<T, S>(watcher:ResourceSource<S>, fetcher:ResourceFetcher<S, T, unknown>) {
-  const [store, setStore] = createStore<PostAndComments[]>([]);
 
   const [ status, { refetch } ] = createResource<T, S>(
     watcher,
     fetcher,
-    {storage: toSignal(store, setStore)}
+    { ...options, storage: toSignal }
   )
 
-  return [status, { mutate: setStore, refetch }] as [Resource<T>, {mutate:  SetStoreFunction<T>, refetch: typeof refetch}]
+  return [status, { mutate: setStore, refetch }] as [Resource<T>, {mutate:  SetStoreFunction<T[]>, refetch: typeof refetch}]
 }
 
 const Post = () => {
@@ -52,8 +46,7 @@ const Post = () => {
   
   const addComment = (pathArr: PathArray[], value: CommentType) => {
     if (getStore() && getStore()?.comments) {
-      // @ts-ignore
-      mutate(0, 'comments', ...pathArr, (existing: CommentType[])=>[value, ...existing])
+      mutate(0, 'comments', ...(pathArr as []), (existing: CommentType[])=>[value, ...existing])
     }
   };
 
