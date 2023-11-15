cbb5c2ba6fb07d3ec3f7263a712222a797f7f850
Thomas Card <tcardlab@gmail.com>
2023-11-15 03:04:35 -0500

    Feat: pass store to resource and replace mutate w/ setStore

--- client/src/pages/Post.tsx
+++ client/src/pages/Post.tsx
@@ -1,5 +1,5 @@
 // Solid Imports
-import { Show, createResource, ErrorBoundary, For, createEffect, Signal } from "solid-js";
+import { Show, createResource, ErrorBoundary, For, createEffect, Signal, Accessor } from "solid-js";
 import { Navigate, useNavigate, useParams } from "@solidjs/router";
 
 // API Imports
@@ -12,48 +12,55 @@ import { formatErrorUrl } from "../utils/utilFunctions";
 import { Comment as CommentType, PostAndComments, PathArray } from "../utils/interfaces";
 //import { createDeepSignal } from "@solid-primitives/resource";
 
-import { createStore } from "solid-js/store";
-
-import type { ResourceSource, ResourceFetcher, Resource } from "solid-js";
-function resourceStore<T, S>(watcher:ResourceSource<S>, fetcher:ResourceFetcher<S, T|void, unknown>) {
-  const [store, setStoreTmp] = createStore<PostAndComments[]>([]);
-  let getStore = (...args: Array<string|number>)=>{ 
-    if(!args.length) return store[0] 
-    
-    // @ts-ignore
-    return args.reduce((a, c) => a?.[c], store[0]) as any
+import { createStore, reconcile, unwrap } from "solid-js/store";
+import type { Store, SetStoreFunction } from "solid-js/store";
+  
+  
+function toSignal<T>(store:Store<T[]>, setStore: SetStoreFunction<T[]>) {
+  function createDeepSignal<T>(): Signal<T | undefined>;
+  function createDeepSignal<T>(value: T): Signal<T>;
+  function createDeepSignal<T>(v?: T): Signal<T> {
+    return [
+      () => store[0] as Accessor<T>,
+      (update: T) => (
+        setStore(0, reconcile(typeof update === "function" ? update(unwrap(store[0])) : v))
+      ),
+    ] as Signal<T>;
   }
-  // @ts-ignore breaks typing... maybe just keep the 0...
-  let setStore = (...args:Array<any>) => setStoreTmp(0, ...args) //typeof setStoreTmp
 
-  const [ status ] = createResource<void, S>(
+  return createDeepSignal
+}
+
+import type { ResourceSource, ResourceFetcher, InitializedResource, Resource } from "solid-js";
+function resourceStore<T, S>(watcher:ResourceSource<S>, fetcher:ResourceFetcher<S, T, unknown>) {
+  const [store, setStore] = createStore<PostAndComments[]>([]);
+
+  const [ status, { refetch } ] = createResource<T, S>(
     watcher,
-    async (v, r)=>{setStore(await fetcher(v, r))}
+    fetcher,
+    {storage: toSignal(store, setStore)}
   )
 
-  return [getStore, setStore, status] as [
-    (...args: Array<string|number>)=>any,
-    (...args: Array<any>)=>any,
-    Resource<void>
-  ]
+  return [status, { mutate: setStore, refetch }] as [Resource<T>, {mutate:  SetStoreFunction<T>, refetch: typeof refetch}]
 }
 
 const Post = () => {
   const params = useParams();
   const navigate = useNavigate();
 
-  let [getStore, setStore, status] = resourceStore<PostAndComments, string>(params.postId, getPostAndComments)
+  let [getStore, { mutate }] = resourceStore<PostAndComments, string>(params.postId, getPostAndComments)
   
   const addComment = (pathArr: PathArray[], value: CommentType) => {
     if (getStore() && getStore()?.comments) {
-      setStore('comments', ...pathArr, (existing: CommentType[])=>[value, ...existing])
+      // @ts-ignore
+      mutate(0, 'comments', ...pathArr, (existing: CommentType[])=>[value, ...existing])
     }
   };
 
   return (
     <ErrorBoundary
       fallback={() => {
-        const { errorMessage, statusCode } = formatErrorUrl(status.error);
+        const { errorMessage, statusCode } = formatErrorUrl(getStore.error);
         return (
           <Navigate
             href={`/${errorPageUrl}?message=${btoa(
