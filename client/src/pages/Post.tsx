b35b3b11a0b6901a193c806e3368e5ec091c9179
Thomas Card <tcardlab@gmail.com>
2023-11-15 01:19:15 -0500

    Feat: functional external resource store

--- client/src/pages/Post.tsx
+++ client/src/pages/Post.tsx
@@ -1,80 +1,75 @@
 // Solid Imports
-import { Show, createResource, ErrorBoundary, For, createEffect } from "solid-js";
+import { Show, createResource, ErrorBoundary, For, createEffect, Signal } from "solid-js";
 import { Navigate, useNavigate, useParams } from "@solidjs/router";
 
 // API Imports
-import { getPostAndComments } from "../apiCalls/CommentSectionCalls";
+//import { getPostAndComments } from "../apiCalls/CommentSectionCalls";
 
 // Local Imports
 import Comment from "../components/Comment";
 import { errorPageUrl } from "../utils/constants";
 import { formatErrorUrl } from "../utils/utilFunctions";
 import { Comment as CommentType, PostAndComments, PathArray } from "../utils/interfaces";
-import { createDeepSignal } from "@solid-primitives/resource";
+//import { createDeepSignal } from "@solid-primitives/resource";
+
+import { createStore, reconcile, unwrap } from "solid-js/store";
+
+import { trpc } from "../utils/api";
 
 const Post = () => {
   const params = useParams();
   const navigate = useNavigate();
 
-  const [
-    singlePost,
-    {mutate}
-  ] = createResource<PostAndComments, string>(
-    params.postId,
-    getPostAndComments,
-    {storage: createDeepSignal}
-  );
+  //let thing;
+  
+  // function createDeepSignal<T>(): Signal<T | undefined>;
+  // function createDeepSignal<T>(value: T): Signal<T>;
+  // function createDeepSignal<T>(v?: T): Signal<T> {
+  //   const [store, setStore] = createStore([v]);
+  //   //thing = setStore
+  //   return [
+  //     () => store[0],
+  //     (update: T, path:[]=[]) => (
+  //       setStore(...[0, ...path], reconcile(typeof update === "function" ? update(unwrap(store[0])) : v)), store[0]
+  //     ),
+  //   ] as Signal<T>;
+  // }
 
-  const addComment = (pathArr: PathArray[], value: CommentType) => {
-    if (singlePost() && singlePost()?.comments) {
-      const postCopy = JSON.parse(JSON.stringify(singlePost()!.comments)) as CommentType[];
-      // console.log("postCopy before", postCopy)
-      let changingObj: CommentType[] | CommentType = postCopy;
-
-      // console.log("changingObj", changingObj);
-      for (let i = 0; i <= pathArr.length - 1; i++) {
-        var elem = pathArr[i];
-        // if(i === 0 && elem === 'comments'){
-        //   const newObj = changingObj['comments'] as CommentType[];
-        //   changingObj = changingObj[elem];
-        // }
-        if (typeof elem === "number" && Array.isArray(changingObj)) {
-          changingObj = changingObj[elem];
-        } else if (
-          elem === "comments" &&
-          "comments" in changingObj
-        ) {
-          changingObj = changingObj[elem];
-        }
-        // if (!changingObj[elem]) changingObj[elem] = {};
-        // changingObj = changingObj[elem];
-      }
-
-      // console.log("pathArr[pathArr.length - 2]", pathArr[pathArr.length - 2]);
-      // console.log("Array.isArray(changingObj)", Array.isArray(changingObj));
-      if (Array.isArray(changingObj)) {
-        changingObj.push(value);
-        mutate((currentPost) => ({ ...currentPost!, comments: postCopy}));
-      }
-      
-      // console.log("path", pathArr);
-      // console.log("value", value);
-      // console.log("changeObj", changingObj);
-      // console.log("postCopy", postCopy);
-    }
-    // changingObj[pathArr[pathArr.length - 1]] = value;
+  const [store, setStoreTmp] = createStore<PostAndComments[]>([]);
+  let getStore = (...args: Array<string|number>)=>{ 
+    if(!args.length) return store[0] 
+    
+    // @ts-ignore
+    return args.reduce((a, c) => a?.[c], store[0]) as any
+  }
+  // @ts-ignore breaks typing... maybe just keep the 0...
+  let setStore = (...args:Array<any>) => setStoreTmp(0, ...args) //typeof setStoreTmp
+  const getPostAndComments = async (post_id: string) => {
+    const response = await trpc.getPostAndComments.query({
+      post_id,
+    });
+    setStore(response)
   };
 
-  createEffect(() => {
-    // console.log("singlePost()", singlePost());
-    // console.log("singlePost isPending", singlePost.loading);
-    // console.log("singlePost latest", singlePost.latest);
-  });
+  const [ status ] = createResource<void, string>(
+    params.postId,
+    getPostAndComments
+    //()=>setStore(getPostAndComments())
+  )
+
+  const addComment = (pathArr: PathArray[], value: CommentType) => {
+    if (getStore() && getStore()?.comments) {
+      setStore('comments', ...pathArr, [
+        value,
+        ...(getStore('comments', ...pathArr) as CommentType[])
+      ])
+    }
+  };
 
   return (
     <ErrorBoundary
       fallback={() => {
-        const { errorMessage, statusCode } = formatErrorUrl(singlePost.error);
+        const { errorMessage, statusCode } = formatErrorUrl(status.error);
         return (
           <Navigate
             href={`/${errorPageUrl}?message=${btoa(
@@ -86,15 +81,15 @@ const Post = () => {
     >
       <div class="flex flex-col items-center justify-center">
         <div class="max-w-3xl w-full">
-          <Show when={singlePost() && singlePost()?.post}>
-            <div class="text-xl font-medium">{singlePost()!.post.title}</div>
-            <div>{singlePost()!.post.description}</div>
+          <Show when={getStore() && getStore()?.post}>
+            <div class="text-xl font-medium">{getStore()!.post.title}</div>
+            <div>{getStore()!.post.description}</div>
           </Show>
 
-          <Show when={singlePost() && singlePost()?.comments}>
+          <Show when={getStore() && getStore()?.comments}>
             <ul class="ml-5">
-              <For each={singlePost()!.comments}>
-                {(comment, index) => <Comment comment={comment} post_id={singlePost()!.post.post_id} pathArr={[index(), "comments"]} addComment={addComment}/>}
+              <For each={getStore()!.comments}>
+                {(comment, index) => <Comment comment={comment} post_id={getStore()!.post.post_id} pathArr={[index(), "comments"]} addComment={addComment}/>}
               </For>
             </ul>
           </Show>
