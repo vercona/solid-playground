b748613c93080872f20add23b86d57832ca76ea3
Thomas Card <tcardlab@gmail.com>
2023-12-15 06:15:05 -0500

    Fix: require breaks typing

--- server/trpc/messages/index.ts
+++ server/trpc/messages/index.ts
@@ -1,16 +1,27 @@
 // NOTE: this file can be auto-generated/updated later
 
-import { router } from "../trpc";
+import { router } from "../trpc"
+
+import createComment from './createComment'
+import createPost from './createUser'
+import createUser from './deleteComment'
+import deleteComment from './getAllUsers'
+import getAllUsers from './createComment'
+import getComment from './getComment'
+import getPost from './getPost'
+import getPostAndComments from './getPostAndComments'
+import getRepliedComments from './getRepliedComments'
+import removeCommentEntirely from './removeCommentEntirely'
 
 export default router({
-  createComment: require('./createComment').default,
-  createPost: require('./createPost').default,
-  createUser: require('./createUser').default,
-  deleteComment: require('./deleteComment').default,
-  getAllUsers: require('./getAllUsers').default,
-  getComment: require('./getComment').default,
-  getPost: require('./getPost').default,
-  getPostAndComments: require('./getPostAndComments').default,
-  getRepliedComments: require('./getRepliedComments').default,
-  removeCommentEntirely: require('./removeCommentEntirely').default
-});
+  createComment,
+  createPost,
+  createUser,
+  deleteComment,
+  getAllUsers,
+  getComment,
+  getPost,
+  getPostAndComments,
+  getRepliedComments,
+  removeCommentEntirely
+})
